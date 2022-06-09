fa8ecead660c07c2ec35aa948f9dda897d3de6eb
redskowa <redskowa@gmail.com>
2022-06-09 08:41:12 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,79 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+
+let apiQuotes = [];
+
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+// Show new Quote
+function newQuote() {
+    showLoadingSpinner();
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    //Check for blank author field
+    if(!quote.author) {
+        authorText.textContent = 'Unknown';
+    }else {
+        authorText.textContent = quote.author;
+    }
+    //Check quote length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    }else {
+        quoteText.classList.remove('long-quote');
+    }
+    // Set Quote, Hide Loader
+    quoteText.textContent = quote.text;
+    removeLoadingSpinner();
+}
+
+// Get Quotes from API
+async function getQuotes() {
+    showLoadingSpinner();
+    const apiURL = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiURL);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        // catch Error here
+    }
+}
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+//On Load
+getQuotes();
+
+
+
+
+//Get new quote from local source file instead of API
+// function newQuote() {
+//     // Pick a random quote from local array
+//     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+//     console.log(quote);
+// }
+
+// newQuote();
